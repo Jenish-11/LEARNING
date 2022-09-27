bf7344b8f75a24111bb2bc935d8ae7311cd90b49
unknown <kljenish@gmail.com>
2022-09-27 17:07:08 +0530

    foodApp

---
+++ 27-09-2022/comorins/output/script.js
@@ -0,0 +1,11 @@
+            let a=1;
+            a="sdfghk"
+            console.log(a);
+            let hobbies=["playing", "gaming", "travelwith friends", ]
+            console.log(hobbies)
+            let companyName=["tata","google","comorins"];
+            a="string"
+            console.log(companyName);
+            alert(companyName);
+            let p ="Abcdefghijklmnopqrstuvwxyz"
+            console.log(p)
