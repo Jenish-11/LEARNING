b339ad08b53d8b3f1ffe8608f49816e3026cee8e
unknown <kljenish@gmail.com>
2022-10-03 20:02:55 +0530

    js

--- 28-09-2022/comorins/output/script1.js
+++ 28-09-2022/comorins/output/script1.js
@@ -1,15 +1,29 @@
 function add(){
+    let d=[];
     let a=document.getElementById("num1").value;
     let b=document.getElementById("num2").value;
     let c=Number(a)+Number(b);
-    alert(c);
+    d.push(c);
+    // alert(c);
+    answ(d);
+}
+function answ(answer_array){
+    let k=0;
+    for(let i=0; i<answer_array.length; i++){
+        k =k+"<li>"+answer_array[i]+"</li>";
+    }
+    document.getElementById("ans").innerHtml=k;
+    
+     
 }
 let list=[]
 function addd(){
     let todo=document.getElementById("items").value;
+    document.getElementById("items").value="";
     list.push(todo);
     console.log(list);
     display(list);
+    
 }
 function display(list_aray){
     let ele="";
